719352b36f4591c349d0c118bd2cb86d1db8c51d
John Ifegwu <johnifegwu@outlook.com>
2021-02-08 05:34:14 +0100

    Restaurant App

--- app/src/main/java/com/mickleentityltdnigeria/resturantapp/ResturantsAdapter.kt
+++ app/src/main/java/com/mickleentityltdnigeria/resturantapp/ResturantsAdapter.kt
@@ -1,6 +1,6 @@
 package com.mickleentityltdnigeria.resturantapp
 
-import android.graphics.BitmapFactory
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,31 +8,46 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.mickleentityltdnigeria.resturantapp.data.FoodItem
+import android.content.res.AssetManager
+import android.graphics.drawable.Drawable
+import java.io.InputStream
 
 
 class ResturantsAdapter(private val foodItems: List<FoodItem>) :
     RecyclerView.Adapter<ResturantsAdapter.ViewHolder>() {
 
+    private val myContext:Context = ApplicationContextProvider.getContext()
+    private val assetManager: AssetManager = myContext.getAssets()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.a_single_resturant_row, parent, false)
-        return ViewHolder(view)
+        return ViewHolder(view,myContext,assetManager)
     }
 
     override fun getItemCount() = foodItems.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val fooditem = foodItems[position];
+        val fooditem = foodItems[position]
         holder.bind(fooditem);
     }
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(view: View, private val myContext:Context,private val assetManager: AssetManager) : RecyclerView.ViewHolder(view) {
         fun bind(fooditem: FoodItem) {
-            itemView.findViewById<ImageView>(R.id.imgFood)
-                .setImageBitmap(BitmapFactory.decodeFile("drawable/" + fooditem.foodUrl ));
-            itemView.findViewById<TextView>(R.id.txtFoodDesc).text = fooditem.foodDesc;
-            itemView.findViewById<TextView>(R.id.txtPrice).text = "$"+ fooditem.foodPrice;
+            try
+            {
+                val ims: InputStream = assetManager.open(fooditem.foodUrl)
+                val d: Drawable = Drawable.createFromStream(ims,null)
+                itemView.findViewById<ImageView>(R.id.imgFood).setImageDrawable(d)
+                itemView.findViewById<TextView>(R.id.txtFoodDesc).text = fooditem.foodDesc
+                itemView.findViewById<TextView>(R.id.txtPrice).text = "$"+ fooditem.foodPrice
+            }catch (e: Exception)
+            {
+               val s = e.message
+            }
+
         }
+
     }
 
 }
