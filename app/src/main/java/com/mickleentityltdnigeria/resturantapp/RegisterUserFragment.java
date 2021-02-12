df6654c282ec8fb3bd3b32e4a914027dc0863896
John Ifegwu <johnifegwu@outlook.com>
2021-02-12 16:41:48 +0100

    Restaurant App

--- app/src/main/java/com/mickleentityltdnigeria/resturantapp/RegisterUserFragment.java
+++ app/src/main/java/com/mickleentityltdnigeria/resturantapp/RegisterUserFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NewUserFragment#newInstance} factory method to
+ * Use the {@link RegisterUserFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewUserFragment extends Fragment {
+public class RegisterUserFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class NewUserFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NewUserFragment() {
+    public RegisterUserFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class NewUserFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewUserFragment.
+     * @return A new instance of fragment RegisterUserFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewUserFragment newInstance(String param1, String param2) {
-        NewUserFragment fragment = new NewUserFragment();
+    public static RegisterUserFragment newInstance(String param1, String param2) {
+        RegisterUserFragment fragment = new RegisterUserFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class NewUserFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_user, container, false);
+        return inflater.inflate(R.layout.fragment_register_user, container, false);
     }
 }
