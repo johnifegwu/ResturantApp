e3a3e99690f1d562c32ca2a9b9d4b43b313458d0
John Ifegwu <johnifegwu@outlook.com>
2021-04-03 00:30:39 +0100

    Restaurant App

--- app/src/main/java/com/mickleentityltdnigeria/resturantapp/ScanQRCodeActivity.java
+++ app/src/main/java/com/mickleentityltdnigeria/resturantapp/ScanQRCodeActivity.java
@@ -1,14 +1,74 @@
 package com.mickleentityltdnigeria.resturantapp;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+
+import javax.xml.transform.Result;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
+    private CodeScanner mCodeScanner;
+
+    int resultCode;
+    Intent data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_q_r_code);
+        CodeScannerView scannerView = findViewById(R.id.scanner_view);
+        mCodeScanner = new CodeScanner(this, scannerView);
+        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull com.google.zxing.Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultCode = Activity.RESULT_OK;
+                        Uri uri = Uri.parse(result.getText());
+                        Intent intent = new Intent(Intent.EXTRA_TEXT, uri);
+                        intent.putExtra("QRCode", result.getText());
+                        data = intent;
+                        finish();
+                    }
+                });
+            }
+        });
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCodeScanner.startPreview();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        resultCode = this.resultCode;
+        data = this.data;
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mCodeScanner.startPreview();
+    }
+
+    @Override
+    protected void onPause() {
+        mCodeScanner.releaseResources();
+        super.onPause();
     }
 }
