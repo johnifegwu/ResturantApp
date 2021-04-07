f7e0578055a22717b5bab68cc612d8e02ab2df12
John Ifegwu <johnifegwu@outlook.com>
2021-04-07 11:08:24 +0100

    Restaurant App

--- app/src/main/java/com/mickleentityltdnigeria/resturantapp/utils/idGen.java
+++ app/src/main/java/com/mickleentityltdnigeria/resturantapp/utils/idGen.java
@@ -16,4 +16,23 @@ public class idGen{
     public String getUUID(){
         return UUID.randomUUID().toString();
     }
+
+    //A function for generating MICKLE-PAY WALLET ID.
+    public String getWalletID(String currencyCode){
+        String wID = currencyCode;
+        int i,x;
+        String s;
+        //block one
+        s = UUID.randomUUID().toString();
+        i = s.length();
+        x = i/2;
+        wID += s.substring(1,x);
+        //block two
+        s = UUID.randomUUID().toString();
+        i = s.length();
+        x = i/2;
+        wID += s.substring(x,i);
+        //
+        return wID.toUpperCase();
+    }
 }
