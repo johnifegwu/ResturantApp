924882632273d2fe729f99b8b3182a5fcb9890e9
John Ifegwu <johnifegwu@outlook.com>
2021-05-22 18:15:23 +0100

    Restaurant App

--- app/src/main/java/com/mickleentityltdnigeria/resturantapp/dalc/FoodItemDalc.java
+++ app/src/main/java/com/mickleentityltdnigeria/resturantapp/dalc/FoodItemDalc.java
@@ -158,8 +158,12 @@ public class FoodItemDalc {
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public int getRandomNumber(int min, int max){
+        if(!(min > max || max - min + 1 > Integer.MAX_VALUE)){
         Random rnd = new Random();
         return rnd.ints(min, max + 1).findFirst().getAsInt();
+        }else{
+            throw new IllegalArgumentException("Integer value out of range.");
+        }
     }
 
     public int getRandomNumber2(int min, int max){
